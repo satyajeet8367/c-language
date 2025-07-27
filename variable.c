d277db3d6b36c09d1567ab444c90b030c9508eb5
satyan8367 <satyanarayanbairwa033@gmail.com>
2025-07-27 14:04:49 +0530

    theory of variables

--- variable.c
+++ variable.c
@@ -1,1 +1,17 @@
-// satyajeet
+/* Variables: Variables are the name of merory location which are used to store value or data
+example: int a;
+a is the variable name which is used to store integer data
+
+Two types of varibales
+1.local variable
+2.global variable
+
+1.Local variable: the variable which is define inside the block of code or function ,is called local variable.
+                  we can not access local variable outside of that block of code or function.
+
+2.Global varibale: the varibale which is define outside of block of code or function is called global varibale
+                   we can access anywhere that variable                  
+
+
+
+*/
